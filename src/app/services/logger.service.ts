fc4530ebe352fdb852fd737ce409ea38c2690157
ashvin jaiswal <ashvin@yujdesigns.com>
2020-01-22 17:11:55 +0530

    Create Logger Service

---
+++ src/app/services/logger.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class LoggerService {
+
+  log(message: string): void{
+    let timeString: String = new Date().toLocaleDateString();
+    console.log(`${message} (${timeString})`);
+  }
+
+  error(message: string): void{
+    console.error(`ERROR: ${message}`);
+  }
+}
